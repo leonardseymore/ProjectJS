7062f3fb4e589c96886692e6b4ba244568158795
Leonard Seymore <leonardseymore@gmail.com>
2013-02-14 12:40:58 +0200

    Basic round off of index and features

--- src/test/appTest.js
+++ src/test/appTest.js
@@ -1,6 +1,6 @@
 eval(loadFile("src/test/lib/env.rhino.1.2.js"));
 eval(loadFile("src/test/lib/jquery-1.8.2.js"));
-eval(loadFile("src/js/util.js"));
+eval(loadFile("src/js/projectjs.js"));
 
 
 testCases(test,
